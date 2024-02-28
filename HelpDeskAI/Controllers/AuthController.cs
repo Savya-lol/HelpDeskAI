9e16321e9c31486944307ae79bb680c4f172eb98
Savya-lol <tropedotuber@gmail.com>
2024-02-28 12:02:53 +0545

    Basic Placeholders Created

---
+++ HelpDeskAI/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using HelpDeskAI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskAI.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(string email, string pass)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(User Model)
+        {
+            return View();
+        }
+    }
+}
---
+++ HelpDeskAI/Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace HelpDeskAI.Models
+{
+    public class User
+    {
+    }
+}
